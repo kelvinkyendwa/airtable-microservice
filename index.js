fa8c542ff6bd0d330cc236022940514d3e7c03c3
Kelvin Kyendwa <kelvinkyendwa@gmail.com>
2021-11-01 19:47:43 +0300

    reset to default

--- index.js
+++ index.js
@@ -6,62 +6,65 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-require('dotenv').config()
+require("dotenv").config();
 
-const records = [];
-app.post('/airtable/user-signups', (req, res) => {
-    const {email, name, country} = req.body;
-    const config = {
-        headers: {
-            "Authorization": `Bearer ${process.env.API_KEY}`,
-            "Content-Type": "application/json"
-
-        }
-    }
-    console.log(req.body);
-    axios.post(`${process.env.AIRTABLE_URL}${process.env.AIRTABLE_BASE}/${process.env.AIRTABLE_SIGNUPS_TABLE}`,
-        {
-            "records": [
-                {
-                    "fields": {
-                        name,
-                        email,
-                        country
-                    }
-                },
-            ]
-        },
-        config
-    ).then(r => console.log("successful data transfer", r.data));
-    res.status(201).send('successful data transfer');
+app.post("/airtable/user-signups", (req, res) => {
+	const { email, name, country } = req.body;
+	const config = {
+		headers: {
+			Authorization: `Bearer ${process.env.API_KEY}`,
+			"Content-Type": "application/json",
+		},
+	};
+	console.log(req.body);
+	axios
+		.post(
+			`${process.env.AIRTABLE_URL}${process.env.AIRTABLE_BASE}/${process.env.AIRTABLE_SIGNUPS_TABLE}`,
+			{
+				records: [
+					{
+						fields: {
+							name,
+							email,
+							country,
+						},
+					},
+				],
+			},
+			config,
+		)
+		.then((r) => console.log("successful data transfer", r.data));
+	res.status(201).send("successful data transfer");
 });
 
-app.post('/airtable/premium', (req, res) => {
-    const {email, name} = req.body;
-    const config = {
-        headers: {
-            "Authorization": `Bearer ${process.env.API_KEY}`,
-            "Content-Type": "application/json"
-
-        }
-    }
-    console.log(req.body);
-    axios.post(`${process.env.AIRTABLE_URL}${process.env.AIRTABLE_BASE}/${process.env.AIRTABLE_PREMIUM_USERS_TABLE}`,
-        {
-            "records": [
-                {
-                    "fields": {
-                        name,
-                        email,
-                    }
-                },
-            ]
-        },
-        config
-    ).then(r => console.log("successful data transfer", r.data));
-    res.status(201).send('successful data transfer');
+app.post("/airtable/premium", (req, res) => {
+	const { email, name } = req.body;
+	const config = {
+		headers: {
+			Authorization: `Bearer ${process.env.API_KEY}`,
+			"Content-Type": "application/json",
+		},
+	};
+	console.log(req.body);
+	axios
+		.post(
+			`${process.env.AIRTABLE_URL}${process.env.AIRTABLE_BASE}/${process.env.AIRTABLE_PREMIUM_USERS_TABLE}`,
+			{
+				records: [
+					{
+						fields: {
+							name,
+							email,
+						},
+					},
+				],
+			},
+			config,
+		)
+		.then((r) => console.log("successful data transfer", r.data));
+	res.status(201).send("successful data transfer");
 });
 
 app.listen(4000, () => {
-    console.log("server listening on 4000");
+	console.log("server listening on 4000");
 });
